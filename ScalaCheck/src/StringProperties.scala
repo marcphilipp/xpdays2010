3c8ad6a72a54247d68e7486f62a577b8728cef78
Marc Philipp <mail@marcphilipp.de>
2010-11-06 17:03:46 +0100

    Einfaches ScalaCheck-Beispiel mit Strings

--- ScalaCheck/src/StringProperties.scala
+++ ScalaCheck/src/StringProperties.scala
@@ -3,18 +3,18 @@ import org.scalacheck.Prop._
 
 object StringProperties extends Properties("String") {
 
-//  property("startsWith") = forAll((a: String, b: String) => (a + b).startsWith(a))
+  property("startsWith") = forAll((a: String, b: String) => (a + b).startsWith(a))
 
-//  property("endsWith") = forAll((a: String, b: String) => (a + b).endsWith(b))
+  property("endsWith") = forAll((a: String, b: String) => (a + b).endsWith(b))
+
+  property("substring") = forAll((a: String, b: String) =>
+    (a + b).substring(a.length) == b)
+
+  property("substring") = forAll((a: String, b: String, c: String) =>
+    (a + b + c).substring(a.length, a.length + b.length) == b)
 	
   // Is this really always true?
   property("concat") = forAll((a: String, b: String) =>
     (a + b).length > a.length && (a + b).length > b.length)
-
-//  property("substring") = forAll((a: String, b: String) =>
-//    (a + b).substring(a.length) == b)
-
-//  property("substring") = forAll((a: String, b: String, c: String) =>
-//    (a + b + c).substring(a.length, a.length + b.length) == b)
     
 }
