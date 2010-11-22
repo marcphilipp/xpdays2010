1b9f204a4aa3a12c97a4365e37d39bd4b18d84ad
Marc Philipp <mail@marcphilipp.de>
2010-11-22 09:07:06 +0100

    Expliziten Generator eingebaut

--- ScalaExperimente/src/funktion_f/FunktionFTest.scala
+++ ScalaExperimente/src/funktion_f/FunktionFTest.scala
@@ -15,18 +15,21 @@ class FunktionFTest extends Spec with Checkers {
   describe("f") {
 
     it("returns 3 for inputs < 5") {
-      check((x : Double) =>
-        (x < 5.0) ==> (f(x) == 3.0) )
-    } 
+      check((x: Double) =>
+        (x < 5.0) ==> (f(x) == 3.0))
+    }
 
-      it("returns 2 for inputs between 5 and 10") {
-      check((x : Double) =>
-        ((5.0 <= x) && (x < 10.0)) ==> (f(x) == 2.0) )
-    } 
+    it("returns 2 for inputs between 5 and 10") {
+      check(
+        //        forAll(Gen.choose(5.0, 10.0))(
+        (x: Double) =>
+          ((5.0 <= x) && (x < 10.0)) ==> (f(x) == 2.0))
+      //)
+    }
 
-      it("returns 4 for inputs >= 10") {
-      check((x : Double) =>
-        (10.0 <= x) ==> (f(x) == 4.0) )
-    } 
-}
+    it("returns 4 for inputs >= 10") {
+      check((x: Double) =>
+        (10.0 <= x) ==> (f(x) == 4.0))
+    }
+  }
 }
